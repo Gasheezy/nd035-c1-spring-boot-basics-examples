703fe83709f0c098edb6d888b48255db39099569
Gasheezy <mungai.gachango@gmail.com>
2021-05-18 16:10:58 +0300

    lesson4-8

--- udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.mvc_basics.controller;
 
+import javax.management.AttributeValueExp;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.engine.AttributeName;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("greetings", new String[]{"Hi", "hello", "goodbye"});
         return "home";
     }
 
